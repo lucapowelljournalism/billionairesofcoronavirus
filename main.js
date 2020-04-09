5bba4981fec36bc2830535fd84d630a507581510
JR Powell <jrpowell@FSPFVFZ929ML414.local>
2020-04-09 11:19:31 -0400

    updated java, added fake text

--- main.js
+++ main.js
@@ -1,25 +1,11 @@
-$("body").on("click", function (e) {
+$("body").on("click", ".tile", function (e) {
 	e.preventDefault();
 
-	this_tile = e.target.id //returns string with ID
-	console.log(this_tile) //
+	const click_face = e.target; // #identifies the div we want as clickface.
+	const target = $(click_face).data("target"); // "gets data attribute #(variablename) from click face."
+	console.log(target) //prints out #jackma
 
-	if (this_tile == "jackma") {
-		$(".jackmaoutput").addClass("active")
-		$(".billgatesoutput").removeClass("active")
-		$(".jeffbezosoutput").removeClass("active");;
-	} else if (this_tile == "billgates") {
-		$(".jackmaoutput").removeClass("active")
-		$(".billgatesoutput").addClass("active")
-		$(".jeffbezosoutput").removeClass("active");;
-	} else if (this_tile == "jeffbezos") {
-		$(".jackmaoutput").removeClass("active")
-		$(".billgatesoutput").removeClass("active")
-		$(".jeffbezosoutput").addClass("active");
-	} else if (this_tile == "clear") {
-		$(".jackmaoutput").removeClass("active")
-		$(".billgatesoutput").removeClass("active")
-		$(".jeffbezosoutput").removeClass("active");
-	}
+	$(".output.active").removeClass("active"); //hide any text that's showing
+	$(target).addClass("active"); //show this text
 
 });
