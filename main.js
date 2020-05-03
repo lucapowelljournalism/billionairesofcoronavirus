0e2ccda1f3882fa3ca67d7cb8a308e736f0b0aea
JR Powell <jrpowell@FSPFVFZ929ML414.local>
2020-05-03 00:22:54 -0400

    more changes

--- main.js
+++ main.js
@@ -3,9 +3,8 @@ $("body").on("click", ".tile", function (e) {
 
 	const click_face = e.target; // #identifies the div we want as clickface.
 	const target = $(click_face).data("target"); // "gets data attribute #(variablename) from click face."
-	console.log(target) //prints out #jackma
+	console.log(target) //prints out "#jackma"
 
 	$(".output.active").removeClass("active"); //hide any text that was showing from a prior billionaire
-	$(target).addClass("active"); //show this text
-
+	$(target).addClass("active"); //add active class, aka display:block, to this text
 });
